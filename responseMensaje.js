d104ae8077486664f3b9373912d42cb411a71fb9
manuelfernando <manuelcamaron@hotmail.com>
2017-08-23 16:59:54 -0500

    update codes

--- responseMensaje.js
+++ responseMensaje.js
@@ -1,9 +1,9 @@
 {
   actualChannel: null,
-  channel: "my_channel_1",
+  channel: "ch1",
   message: "Hello World!",
   publisher: "pn-58e1a647-3e8a-4c7f-bfa4-e007ea4b2073",
-  subscribedChannel: "my_channel_1",
+  subscribedChannel: "ch1",
   subscription: null,
   timetoken: "14966804541029440"
 }
--- responseStatus.js
+++ responseStatus.js
@@ -1,8 +1,8 @@
 {
   category: 'PNConnectedCategory',
   operation: 'PNSubscribeOperation',
-  affectedChannels: ['my_channel_1'],
-  subscribedChannels: ['my_channel_1'],
+  affectedChannels: ['ch1'],
+  subscribedChannels: ['ch1'],
   affectedChannelGroups: [],
   lastTimetoken: '14974492380756600',
   currentTimetoken: '14974492384874375'
