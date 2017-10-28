6c1840e25a5b6efdbe94dd05d9ac32de36291b54
manuelfernando <manuelcamaron@hotmail.com>
2017-10-27 19:09:53 -0500

    add example of delete message from history

---
+++ deleteMessages.js
@@ -0,0 +1,10 @@
+pubnub.deleteMessages(
+	{
+  		channel: 'ch1',
+  		start: '15088506076921021',
+  		end: '15088532035597390'
+	},
+	(result) => {
+		console.log(result);
+	}
+);
---
+++ responseDeleteMessages.js
@@ -0,0 +1,5 @@
+{
+	error: false,
+	operation: 'PNDeleteMessagesOperation',
+	statusCode: 200
+}
