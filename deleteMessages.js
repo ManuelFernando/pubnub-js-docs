8c4f9dadd5b9cb2ddddfde4e57eb61b3fa1b3026
manuelfernando <manuelcamaron@hotmail.com>
2017-10-27 19:17:01 -0500

    update code

--- deleteMessages.js
+++ deleteMessages.js
@@ -1,10 +1,10 @@
 pubnub.deleteMessages(
   {
-  	channel: 'ch1',
-  	start: '15088506076921021',
-  	end: '15088532035597390'
+    channel: 'ch1',
+    start: '15088506076921021',
+    end: '15088532035597390'
   },
   (result) => {
-	console.log(result);
+    console.log(result);
   }
 );
